c8fced22ad5d1583e210331593fbc7855341c36f
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2020-10-31 18:43:21 -0300

    adding all the files to the repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [appState, changeState] = useState({
+    activeObject: null,
+    colores: [{color: 'rojo'}, {color: 'amarillo'}, {color: 'verde'}]
+  });
+
+
+function toggleActive(index){
+  changeState({ ...appState, activeObject: appState.colores[index]})
+}
+
+function toggleActiveStyles(index){
+  if (appState.colores[index] === appState.activeObject) {
+    return "glow";
+  } else {
+    return "notglow"
+  }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="semaforo">
+        <div className="cable"></div>
+        <div className="container">
+            {appState.colores.map((e,index) => (
+              <div key={index} className={`circle ${e.color} ${toggleActiveStyles(index)}`} onClick={() => {toggleActive(index);}} ></div>
+            ))}
+        </div>
     </div>
   );
 }
